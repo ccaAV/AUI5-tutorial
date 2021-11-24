e2b6f327601dd8fadcf8da276496b5f15056fa3c
Clement Caylux <cca@activeviam.com>
2021-11-24 12:54:51 +0100

    adding Content Editor

---
+++ src/MapContentEditor.tsx
@@ -0,0 +1,12 @@
+import {EditorProps, useDataModel} from "@activeviam/activeui-sdk";
+import React, {FC} from "react";
+
+
+export const MapContentEditor: FC<EditorProps> = (props) => {
+
+    // const dataModel = useDataModel("my-server");
+    // const measures = dataModel? dataModel.catalogs[0].cubes[0].measures: [];
+
+
+    return <div>I am the map's content editor!</div>
+}
--- src/PluginWidgetMap.ts
+++ src/PluginWidgetMap.ts
@@ -1,11 +1,13 @@
 import {WidgetPlugin} from "@activeviam/activeui-sdk";
 import {IconWorld} from "./IconWorld";
 import { Map } from "./Map"
+import {MapContentEditor} from "./MapContentEditor";
 
 const widgetKey = "map"
 
 export const pluginWidgetMap: WidgetPlugin = {
     Component: Map,
+    contentEditor: MapContentEditor,
     Icon: IconWorld,
     initialState: {
         widgetKey
