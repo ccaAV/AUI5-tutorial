f9348ccd5cc4e1546dab382a8b77821c6660bc92
Clement Caylux <cca@activeviam.com>
2021-11-24 11:15:25 +0100

    adding map

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,6 +1,33 @@
-import {useQueryResult, WidgetPluginProps} from "@activeviam/activeui-sdk";
+import {CellSet, useQueryResult, WidgetPluginProps} from "@activeviam/activeui-sdk";
 import {Spin} from "antd";
 import React, {FC} from 'react'
+import Plot from "react-plotly.js";
+
+
+const getCountriesAndValues = (data?: CellSet): [string[], number[]] => {
+    if (!data) {
+        return [[],[]];
+    } else {
+        const [columnsAxis, rowsAxis] = data.axes;
+        const numberOfYears = columnsAxis.positions.length;
+        const numberOfCountries = rowsAxis.positions.length;
+
+        const valueForSelectedYear = new Array(numberOfCountries).fill(null);
+        data.cells.forEach(cell => {
+            const rowIndex = Math.floor(cell.ordinal / numberOfYears);
+            let columnIndex = cell.ordinal % numberOfYears;
+            const year = columnsAxis.positions[columnIndex][0].captionPath[0];
+
+            if (year === '2019') {
+                valueForSelectedYear[rowIndex] = cell.value;
+            }
+        });
+
+        return [rowsAxis.positions.map(position => position[0].captionPath[2]), valueForSelectedYear]
+
+    }
+}
+
 
 export const Map: FC<WidgetPluginProps> = (props) => {
 
@@ -30,33 +57,52 @@ export const Map: FC<WidgetPluginProps> = (props) => {
         return null;
     }
 
-    let [columnAxis, rowsAxis] = data.axes;
-    let numberOfColumns = columnAxis.positions.length;
+    let [columnsAxis, rowsAxis] = data.axes;
+    let numberOfColumns = columnsAxis.positions.length;
 
-    console.log(data);
+    let [countries, values] = getCountriesAndValues(data);
+    console.log(isLoading, data, countries, values);
+
+    // return (
+    //     <table>
+    //         <tr>
+    //             <th/>
+    //             {columnsAxis.positions.map((position, columnIndex) => (
+    //                 <th key={columnIndex}> {position[0].captionPath[0]}</th>
+    //             ))}
+    //         </tr>
+    //         {rowsAxis.positions.map((position, rowIndex) => {
+    //             const tableCells: JSX.Element[] = [];
+    //             tableCells.push(<td key={0}>{position[0].captionPath[2]}</td>);
+    //             for (let columnIndex = 0; columnIndex < numberOfColumns ; columnIndex++) {
+    //                 const cellIndex = rowIndex * numberOfColumns + columnIndex;
+    //                 const dataCell = data.cells[cellIndex];
+    //                 tableCells.push(
+    //                     <td key={columnIndex + 1}>{dataCell?.formattedValue}</td>
+    //                 )
+    //             }
+    //
+    //             return <tr key={rowIndex}>{tableCells}</tr>
+    //
+    //         })}
+    //     </table>
+    // )
 
     return (
-        <table>
-            <tr>
-                <th/>
-                {columnAxis.positions.map((position, columnIndex) => (
-                    <th key={columnIndex}> {position[0].captionPath[0]}</th>
-                ))}
-            </tr>
-            {rowsAxis.positions.map((position, rowIndex) => {
-                const tableCells: JSX.Element[] = [];
-                tableCells.push(<td key={0}>{position[0].captionPath[2]}</td>);
-                for (let columnIndex = 0; columnIndex < numberOfColumns ; columnIndex++) {
-                    const cellIndex = rowIndex * numberOfColumns + columnIndex;
-                    const dataCell = data.cells[cellIndex];
-                    tableCells.push(
-                        <td key={columnIndex + 1}>{dataCell?.formattedValue}</td>
-                    )
-                }
-
-                return <tr key={rowIndex}>{tableCells}</tr>
-
-            })}
-        </table>
-    )
+        <div style={{...props.style, height: "100%",}}>
+         <Plot
+             data={[
+                 {
+                     type: "choropleth",
+                     locationmode: "country names",
+                     locations: countries,
+                     z: values,
+                     text: countries,
+                     // @ts-ignore
+                     autocolorscale: true,
+                 },
+           ]}
+         />
+       </div>
+    );
 }
