3377e90cb3c28cadc046b13adcb1db402bd70a45
Clement Caylux <cca@activeviam.com>
2021-11-24 09:40:53 +0100

    starter

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,7 +1,18 @@
-import {WidgetPluginProps} from "@activeviam/activeui-sdk";
+import {useQueryResult, WidgetPluginProps} from "@activeviam/activeui-sdk";
 import React, {FC} from 'react'
 
 export const Map: FC<WidgetPluginProps> = (props) => {
+
+    let {isLoading, data, error} = useQueryResult({
+        serverKey: "my-server",
+        queryId: "abc",
+        query: {
+            mdx: "SELECT FROM [Green-growth]"
+        }
+    });
+
+    console.log(isLoading, data);
+
     return (
         <div style={props.style}>
             Hello World!
