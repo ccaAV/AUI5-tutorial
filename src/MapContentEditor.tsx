96ba12b8ef0c987b55398675103aa8c7c6a6ebaa
Clement Caylux <cca@activeviam.com>
2021-11-24 13:09:14 +0100

    updating Map content editor

--- src/MapContentEditor.tsx
+++ src/MapContentEditor.tsx
@@ -1,12 +1,18 @@
-import {EditorProps, useDataModel} from "@activeviam/activeui-sdk";
+import {EditorProps, Tree, useDataModel} from "@activeviam/activeui-sdk";
 import React, {FC} from "react";
 
 
 export const MapContentEditor: FC<EditorProps> = (props) => {
 
-    // const dataModel = useDataModel("my-server");
-    // const measures = dataModel? dataModel.catalogs[0].cubes[0].measures: [];
+    const dataModel = useDataModel("my-server");
+    const measures = dataModel? dataModel.catalogs[0].cubes[0].measures: [];
 
 
-    return <div>I am the map's content editor!</div>
+    return (
+        <Tree
+            isSearchVisible={true}
+            searchPlaceholder={"Search Measures"}
+            value={measures}
+        />
+    )
 }
