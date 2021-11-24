2918e6e33356a447e247c259224b9a1e330a56c9
Clement Caylux <cca@activeviam.com>
2021-11-24 09:42:37 +0100

    adding spin

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,17 +1,26 @@
 import {useQueryResult, WidgetPluginProps} from "@activeviam/activeui-sdk";
+import {Spin} from "antd";
 import React, {FC} from 'react'
 
 export const Map: FC<WidgetPluginProps> = (props) => {
 
     let {isLoading, data, error} = useQueryResult({
         serverKey: "my-server",
-        queryId: "abc",
+        queryId: props.queryId,
         query: {
-            mdx: "SELECT FROM [Green-growth]"
+            mdx: `SELECT
+                Crossjoin(
+                  [Green-growth].[Year].[Year].Members,
+                  [Measures].[Real GDP per capita (USD).MEAN]
+                ) ON COLUMNS,
+                [Countries].[Country].[Country_Name].Members ON ROWS
+                FROM [Green-growth]`
         }
     });
 
-    console.log(isLoading, data);
+    if (isLoading) {
+        return <Spin/>
+    }
 
     return (
         <div style={props.style}>
