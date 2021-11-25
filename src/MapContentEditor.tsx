76e6ad6827b883add58386848236b584487d462f
Clement Caylux <cca@activeviam.com>
2021-11-25 11:08:09 +0100

    solution

--- src/MapContentEditor.tsx
+++ src/MapContentEditor.tsx
@@ -1,17 +1,59 @@
-import {EditorProps, Tree, useDataModel} from "@activeviam/activeui-sdk";
+import {
+    addMeasure,
+    EditorProps,
+    getMeasures,
+    Measure,
+    removeMeasure,
+    Tree,
+    useDataModel
+} from "@activeviam/activeui-sdk";
 import React, {FC} from "react";
+import {MapWidgetState} from "./map.types";
 
 
-export const MapContentEditor: FC<EditorProps> = (props) => {
+export const MapContentEditor: FC<EditorProps<MapWidgetState>> = (props) => {
 
     const dataModel = useDataModel("my-server");
-    const measures = dataModel? dataModel.catalogs[0].cubes[0].measures: [];
+    const cube = dataModel? dataModel.catalogs[0].cubes[0] :null;
+    const {mdx} = props.widgetState.query;
+    const currentMeasureName = getMeasures(mdx)[0].measureName;
+    const measures = (cube? cube.measures: []).map(measure => ({
+        ...measure,
+        isDisabled: measure.name === currentMeasureName
+    }));
+
+    const handleMeasureClicked = (measure: Measure & {isDisabled: boolean}) => {
+
+        if (!cube) {
+            return;
+        }
+
+        let parsedWithoutMeasure = removeMeasure(mdx, {
+            cube,
+            measureName: currentMeasureName
+        });
+
+        let parsedWithNewMeasure = addMeasure(parsedWithoutMeasure, {
+            cube,
+            measureName: measure.name
+        });
+
+        props.onWidgetChange({
+            ...props.widgetState,
+            query: {
+                mdx: parsedWithNewMeasure
+            }
+        })
+    }
+
+
 
 
     return (
         <Tree
             isSearchVisible={true}
             searchPlaceholder={"Search Measures"}
+            onClick={handleMeasureClicked}
             value={measures}
         />
     )
--- src/PluginWidgetMap.ts
+++ src/PluginWidgetMap.ts
@@ -1,4 +1,4 @@
-import {WidgetPlugin} from "@activeviam/activeui-sdk";
+import {MdxSelect, parse, WidgetPlugin} from "@activeviam/activeui-sdk";
 import {IconWorld} from "./IconWorld";
 import { Map } from "./Map"
 import {MapWidgetState} from "./map.types";
@@ -12,13 +12,15 @@ export const pluginWidgetMap: WidgetPlugin<MapWidgetState> = {
     Icon: IconWorld,
     initialState: {
         widgetKey,
-        mdx: `SELECT
+        query: {
+            mdx: parse<MdxSelect>(`SELECT
             Crossjoin(
               [Green-growth].[Year].[Year].Members,
               [Measures].[Real GDP per capita (USD).MEAN]
             ) ON COLUMNS,
             [Countries].[Country].[Country_Name].Members ON ROWS
-            FROM [Green-growth]`
+            FROM [Green-growth]`)
+    }
 
     },
     key: widgetKey,
